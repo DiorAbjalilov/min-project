5b7eb508df6fd55970536d05af5d190654a33acd
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-15 10:24:03 +0500

    add

--- src/components/loginPage/LoginPage.js
+++ src/components/loginPage/LoginPage.js
@@ -1,37 +1,32 @@
 import React from "react";
 import "./login.css";
 const LoginPage = () => {
-  /*
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
-    */
+  const ClickHandleSignUp = () => {
+    const container = document.querySelector("#container");
+    container.classList.add("right-panel-active");
+  };
+  const ClickHandleSignIn = () => {
+    const container = document.querySelector("#container");
+    container.classList.remove("right-panel-active");
+  };
   return (
     <>
-      <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
-      <div class="container" id="container">
-        <div class="form-container sign-up-container">
+      {/* <h2>Weekly Coding Challenge #1: Sign in/up Form</h2> */}
+      <div className="container" id="container">
+        <div className="form-container sign-up-container">
           <form action="#">
             <h1>Create Account</h1>
-            <div class="social-container">
-              <a href="#" class="social">
-                <i class="fab fa-facebook-f"></i>
+            {/* <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-google-plus-g"></i>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
               </a>
-            </div>
+            </div> */}
             <span>or use your email for registration</span>
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
@@ -39,20 +34,20 @@ signInButton.addEventListener('click', () => {
             <button>Sign Up</button>
           </form>
         </div>
-        <div class="form-container sign-in-container">
+        <div className="form-container sign-in-container">
           <form action="#">
             <h1>Sign in</h1>
-            <div class="social-container">
-              <a href="#" class="social">
-                <i class="fab fa-facebook-f"></i>
+            {/* <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-google-plus-g"></i>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
               </a>
-            </div>
+            </div> */}
             <span>or use your account</span>
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
@@ -60,44 +55,27 @@ signInButton.addEventListener('click', () => {
             <button>Sign In</button>
           </form>
         </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-left">
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button class="ghost" id="signIn">
+              <button className="ghost" id="signIn" onClick={ClickHandleSignIn}>
                 Sign In
               </button>
             </div>
-            <div class="overlay-panel overlay-right">
+            <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button class="ghost" id="signUp">
+              <button className="ghost" id="signUp" onClick={ClickHandleSignUp}>
                 Sign Up
               </button>
             </div>
           </div>
         </div>
       </div>
-
-      <footer>
-        <p>
-          Created with <i class="fa fa-heart"></i> by
-          <a target="_blank" href="https://florin-pop.com">
-            Florin Pop
-          </a>
-          - Read how I created this and how you can join the challenge
-          <a
-            target="_blank"
-            href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/"
-          >
-            here
-          </a>
-          .
-        </p>
-      </footer>
     </>
   );
 };
