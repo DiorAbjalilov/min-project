cf9d7a31dfe6b47d6f087ee8b8c27978a3bd4d4d
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-03-25 14:27:00 +0500

    aadd

--- src/components/Components.js
+++ src/components/Components.js
@@ -1,6 +1,11 @@
 import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import LoginAndRes from "./loginPage/LoginAndRes";
+import MainLayout from "./userPage/Components/layout/MainLayout";
+import Blank from "./userPage/Components/pages/Blank";
+import Dashboard from "./userPage/Components/pages/Dashboard";
+import "./userPage/Components/assets/libs/boxicons-2.1.1/css/boxicons.min.css";
+import "./userPage/Components/scss/App.scss";
 const Components = () => {
   let navigate = useNavigate();
   useEffect(() => {
@@ -13,9 +18,14 @@ const Components = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Default page</h1>} />
         <Route path="/login" element={<LoginAndRes />} />
         <Route path="/user" element={<h1>user page</h1>} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="todo" element={<Blank />} />
+          <Route path="gallery" element={<Blank />} />
+          <Route path="settings" element={<Blank />} />
+        </Route>
       </Routes>
     </>
   );
---
+++ src/components/userPage/Components/configs/sidebarNav.js
@@ -0,0 +1,28 @@
+const sidebarNav = [
+  {
+    link: "/",
+    section: "home",
+    icon: <i className="bx bx-home-alt"></i>,
+    text: "Home",
+  },
+  {
+    link: "/todo",
+    section: "todo",
+    icon: <i className="bx bx-receipt"></i>,
+    text: "Todo",
+  },
+  {
+    link: "/gallery",
+    section: "gallery",
+    icon: <i className="bx bx-cube"></i>,
+    text: "Gallery",
+  },
+  {
+    link: "/settings",
+    section: "settings",
+    icon: <i className="bx bx-cog"></i>,
+    text: "Settings",
+  },
+];
+
+export default sidebarNav;
