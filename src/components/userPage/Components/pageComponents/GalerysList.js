188bc6bcdc16d6ada7feea2156e9ad38068cfc37
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-04-08 15:55:04 +0500

    fix galery list

---
+++ src/components/userPage/Components/pageComponents/GalerysList.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Loading from "./loading/Loading";
+import "./galeryStyle.css";
+
+const GalerysList = () => {
+  const isLoader = useSelector((state) => state.posts.isLoading);
+  if (isLoader) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+  return (
+    <>
+      <div id="container">
+        <div className="polaroid">
+          <img
+            className="photo"
+            src="https://images.unsplash.com/photo-1601288496920-b6154fe3626a?w=634"
+            alt=""
+          />
+          <div className="capition">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              laudantium nesciunt repudiandae blanditiis nam aliquam itaque
+              doloremque nostrum veniam! Voluptate quas in nisi illo natus
+              explicabo ullam aut nostrum. Iusto.
+            </p>
+          </div>
+        </div>
+        <div className="polaroid">
+          <img
+            className="photo"
+            src="https://images.unsplash.com/photo-1604004555489-723a93d6ce74?w=600"
+            alt=""
+          />
+          <div className="capition">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              laudantium nesciunt repudiandae blanditiis nam aliquam itaque
+              doloremque nostrum veniam! Voluptate quas in nisi illo natus
+              explicabo ullam aut nostrum. Iusto.
+            </p>
+          </div>
+        </div>
+        <div className="polaroid">
+          <img
+            className="photo"
+            src="https://images.unsplash.com/photo-1612886999633-1a6cfbac68f1?&w=600"
+            alt=""
+          />
+          <div className="capition">
+            <h2>Title</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+              laudantium nesciunt repudiandae blanditiis nam aliquam itaque
+              doloremque nostrum veniam! Voluptate quas in nisi illo natus
+              explicabo ullam aut nostrum. Iusto.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default GalerysList;
--- src/components/userPage/Components/pages/GalleryList.jsx
+++ src/components/userPage/Components/pages/GalleryList.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import GalerysList from "../pageComponents/GalerysList";
 
 const GalleryList = () => {
-  return <div>GalleryList</div>;
+  return (
+    <>
+      <div className="container">
+        <nav
+          className="navbar navbar-light"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <h5>My Gallery</h5>
+          <div style={{ display: "flex" }}>
+            <button
+              type="button"
+              style={{ marginTop: "0", fontSize: "20px", marginRight: "10px" }}
+              className="btn btn-light"
+            >
+              <i class="bx bx-refresh"></i>
+            </button>
+            <button
+              type="button"
+              style={{ marginTop: "0", fontSize: "20px" }}
+              className="btn btn-success"
+            >
+              <i className="bx bxs-add-to-queue"></i>
+            </button>
+          </div>
+        </nav>
+        <GalerysList />
+      </div>
+    </>
+  );
 };
 
 export default GalleryList;
